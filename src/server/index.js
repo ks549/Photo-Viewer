18613639aa24adb0c0caa696b64fe2210cd589ed
Keerthana Sekar <ks549@njit.edu>
2020-05-19 19:33:37 -0400

    'deploy'

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ const galleryService = require('./service');
 const path = require('path');
 
 app.use(cors({ origin: true }));
-if (process.env.NODE_ENV === 'production') {
+
 
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
@@ -14,7 +14,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname,  "dist", "index.html"));
 });
 
-}
 
 app.get('/api/get-gallery-info', (req, res) => {
   console.log(req.query);
